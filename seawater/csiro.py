6334fe65f5ebaaf51ac3504e3bd3b9ff80a28f77
ocefpaf <devnull@localhost>
2010-12-11 15:12:03 +0000

    auto-commit

--- seawater/csiro.py
+++ seawater/csiro.py
@@ -736,46 +736,19 @@ def cndr(s, t, p):
     s, t, p = np.asarray(s), np.asarray(t), np.asarray(p)
 
     T68 = T68conv(t)
-    DT  = T68 - 15
 
-    Rx  = (s/35.0)**0.5 # first guess at Rx = sqrt(Rt)
-    SInc  = sals(Rx*Rx, t)
-
-    # Do a Newton-Raphson iteration for inverse interpolation of rt from s.
-    for n in range(100): # TODO: is a 100 important? changed to 10?
-        Rx   = Rx + (s - SInc) / salds(Rx, DT)
-        SInc = sals(Rx*Rx, t)
-        DELS = abs(SInc - s)
-        DELS = np.array(DELS) # TODO: "any" is an array method
-        if (DELS.any() < 1.0E-4):
+    # DO A NEWTON-RAPHSON ITERATION FOR INVERSE INTERPOLATION OF Rt FROM S.
+    Rx = np.sqrt( s / 35.0 ) # first guess at Rx = sqrt(Rt)
+    SInc = sals( Rx**2, t ) # S INCrement (guess) from Rx
+    iloop = 0
+    end_loop = 0
+    while True:
+        Rx = Rx + ( s - SInc ) / salds( Rx, T90conv(t) - 15 )
+        SInc = sals( Rx**2, t )
+        iloop = iloop + 1
+        dels = np.abs( SInc - s )
+        if not (dels.all() > 1.0e-10) & (iloop < 100):
             break
-    """ original matlab TODO: implement this above and check the difference
-    for i = 1:ms
-        for j = 1:ns
-            %---------------------------------------------------------------------
-            % DO A NEWTON-RAPHSON ITERATION FOR INVERSE INTERPOLATION OF Rt FROM S.
-            %---------------------------------------------------------------------
-            S_loop  = S(i,j) % S in the loop
-            T_loop  = T(i,j) % T in the loop
-            Rx_loop = sqrt(S_loop/35.0)                % first guess at Rx = sqrt(Rt)
-            SInc    = sw_sals(Rx_loop.*Rx_loop,T_loop) % S INCrement (guess) from Rx
-            iloop    = 0
-            end_loop = 0
-            while ~end_loop
-                Rx_loop = Rx_loop + (S_loop - SInc) ./ sw_salds(Rx_loop,T_loop/1.00024 - 15)
-                SInc    = sw_sals(Rx_loop.*Rx_loop,T_loop)
-                iloop   = iloop + 1
-                dels    = abs(SInc-S_loop)
-                if (dels>1.0e-10 & iloop<100)
-                    end_loop = 0
-                else
-                    end_loop = 1
-                end %if
-            end %while
-            Rx(i,j) = Rx_loop
-        end %for j
-    end %for i
-    """
 
     # ONCE Rt FOUND, CORRESPONDING TO EACH (S,T) EVALUATE R
     # eqn(4) p.8 UNESCO 1983
@@ -798,7 +771,7 @@ def cndr(s, t, p):
     #Rtrt  = rt * Rt # TODO: unused in the code, but present in the original
     D     = B - A * rt * Rt
     E     = rt * Rt * A * ( B + C )
-    r     = ( abs( D**2 + 4 * E ) )**0.5 - D
+    r     = np.sqrt( np.abs( D**2 + 4 * E ) ) - D
     r     = 0.5 * r/A
     return r
 
