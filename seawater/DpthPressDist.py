12ba88e6841bc6643edd49da7ae74e318a528e8c
Filipe Fernandes <ocefpaf@gmail.com>
2011-01-25 14:03:29 -0500

    Renamed DepthPressure.py to DpthPressDist.py Added "distance" to DpthPressDist.py

--- seawater/DpthPressDist.py
+++ seawater/DpthPressDist.py
@@ -2,7 +2,7 @@ import numpy as np
 from seawater import constants as cte
 from seawater import library as lib
 
-def  z_from_p(p, lat): # Also in DepthPressure.py
+def  z_from_p(p, lat):
     """
     Calculates height from sea pressure using the computationally-efficient 25-term expression for density in terms of SA, CT and p.
 
@@ -67,7 +67,7 @@ def  z_from_p(p, lat): # Also in DepthPressure.py
 
     return z
 
-def  p_from_z(z, lat): # Also in DepthPressure.py
+def  p_from_z(z, lat): # Also in DpthPressDist.py
     """
     Calculates sea pressure from height using computationally-efficient 25-term expression for density, in terms of SA, CT and p.
 
@@ -135,7 +135,7 @@ def  p_from_z(z, lat): # Also in DepthPressure.py
 
     return p
 
-def grav(lat, p=0): # Also in DepthPressure.py
+def grav(lat, p=0):
     """
     Calculates acceleration due to gravity as a function of latitude and as a function of pressure in the ocean.
 
@@ -194,7 +194,160 @@ def grav(lat, p=0): # Also in DepthPressure.py
     grav = gs * (1 - cte.gamma * z) # z is the height corresponding to p
     return grav
 
-#class DepthPressure: #TODO: an attempt to create a class for Depth pressure conversions
+def distance(lon, lat, p=None):
+    """
+    Calculates the distance in metres between successive points in the vectors lon and lat, computed using the Haversine formula on a spherical earth of radius 6,371 km, being the radius of a sphere having the same volume as Earth. For a sperical Earth of radius 6,371,000 m, one nautical mile is 1,853.2488 m, thus one degree of latitude is 111,194.93 m.
+
+    Parameters
+    ----------
+    lon : array_like
+          decimal degrees east [0..+360] or [-180 ... +180]
+    lat : array_like
+          latitude in decimal degrees north [-90..+90]
+    p : number or array_like. Default p = 0
+        pressure [db]
+
+    Returns
+    -------
+    dist: array_like
+          distance between points on a spherical Earth at pressure (p) [m]
+
+    See Also
+    --------
+    TODO
+
+    Notes
+    -----
+    Distances are probably good to better than 1% of the "true" distance on the ellipsoidal earth.
+
+    Examples
+    --------
+    >>> import seawater.DpthPressDist as gsw
+    lon = [142, 183, 20]
+    lat = [11, 9.5, 59]
+    p = [[0., 0., 0.,],[500., 500., 500.,]]
+    gsw.distance(lon, lat, p)
+    TODO
+
+    References
+    ----------
+    .. [1] http://www.eos.ubc.ca/~rich/map.html
+
+    Modifications:
+    2000-11-06. Rich Pawlowicz
+    2010-07-28. Paul Barker and Trevor McDougall
+    2010-12-09. Filipe Fernandes, Python translation from gsw toolbox.
+    """
+
+    #NOTE: dtype=np.float64 is need because the test data is uint8
+    lon, lat = np.asarray(lon, dtype=np.float64), np.asarray(lat, dtype=np.float64)
+
+    if (lon.size == 1) & (lat.size == 1):
+        raise NameError('more than one point is needed to compute distance')
+    elif lon.ndim != lat.ndim:
+        raise NameError('lon, lat must have the same dimension')
+
+    if (lon.size == 1) & (lat.size != 1): # fill lon with lat size
+        lon = lib.check_dim(lon, lat)
+    elif (lat.size == 1) & (lon.size != 1): # fill lat with lon size
+        lat = lib.check_dim(lat, lon)
+
+    if (lon.ndim == 1) & (lat.ndim == 1): #NOTE: Ugly way to matlabsize it
+        lon = lon[np.newaxis,:]
+        lat = lat[np.newaxis,:]
+
+    # check for lon/lat and p dimensions
+    if p == None:
+        p = np.zeros( lon.shape )
+    else:
+        p = np.asarray(p)
+
+    if p.ndim > lat.ndim:
+        lon = lib.check_dim(lon, p)
+        lat = lib.check_dim(lat, p)
+    elif p.ndim == 1:
+        p = lib.check_dim(p, lon)
+
+    dlon = np.deg2rad( np.diff(lon, axis=1) )
+    dlat = np.deg2rad( np.diff(lat, axis=1) )
+
+    a = ( np.sin(dlat/2.) )**2 + np.cos( np.deg2rad( lat[:,:-1] ) ) * np.cos( np.deg2rad( lat[:,1:] ) ) * ( np.sin(dlon/2.) )**2
+
+    angles = 2. * np.arctan2( np.sqrt(a), np.sqrt(1-a) )
+
+    p_mid = 0.5 * (   p[:,0:-1] +   p[:,0:-1] )
+    lat_mid = 0.5 * ( lat[:,:-1] + lat[:,1:] )
+
+    z = z_from_p(p_mid, lat_mid) #NOTE: z is height and is negative in th ocean
+
+    distance = (cte.a + z) * angles
+
+    return distance
+
+if __name__=='__main__':
+    try:
+        import cPickle as pickle
+    except:
+        import pickle
+    import numpy as np
+    import scipy.io as sio
+
+    """ load test data """
+    class Dict2Struc(object):
+        """ all the variables from a dict in a "matlab-like-structure" """
+        def __init__(self, adict):
+            self.__dict__.update(adict)
+
+    data = pickle.load( open('gsw_cv.pkl','rb') )
+    gsw_cv = Dict2Struc(data) # then type dat.<tab> to navigate through your variables
+
+    def test_print(method, comp_value=None):
+        """
+        Run a function test mimicking the original logic. This is done to allow for a direct comparison of the result from the Matlab to the python package.
+        """
+
+        if comp_value is None:
+            comp_value = method
+
+        # test for floating differences with: computed - check_value >= defined_precision
+        try:
+            exec( "unequal = (gsw_cv." +comp_value+ " - " +method+ " ) >= gsw_cv." +comp_value+ "_ca")
+        except:
+            print "unequal = (gsw_cv." +comp_value+ " - " +method+ " ) >= gsw_cv." +method+ "_ca"
+            exec( "unequal = (gsw_cv." +comp_value+ " - " +method+ " ) >= gsw_cv." +method+ "_ca")
+
+        width = 23
+        if unequal.any():
+            print "%s: Failed" % method.rjust(width)
+        else:
+            # test if check value is identical to computed value
+            if eval( "( gsw_cv." +comp_value+ "[~np.isnan(gsw_cv."+comp_value+")] == " +method+ "[~np.isnan("+method+")] ).all()" ):
+                print "%s: Passed" % method.rjust(width)
+            else:
+                # test for differences in case their aren't equal. This is an attempt to place all tests together (i.e. term25 and small float differences that will appear)
+                exec("nmax = ( gsw_cv."+comp_value+" - "+method+" )[~np.isnan(gsw_cv."+comp_value+")].max()")
+                exec("nmin = ( gsw_cv."+comp_value+" - "+method+" )[~np.isnan(gsw_cv."+comp_value+")].min()")
+                print "%s: Passed, but small diff ranging from: %s to %s" % ( method.rjust(width), nmax, nmin)
+
+    import seawater.DpthPressDist as gsw
+
+    """ z_from_p """
+    z_from_p = gsw.z_from_p(gsw_cv.p_chck_cast, gsw_cv.lat_chck_cast)
+    test_print("z_from_p")
+
+    """ p_from_z """ #NOTE: show diff not present in the original
+    p_from_z = gsw.p_from_z( z_from_p, gsw_cv.lat_chck_cast )
+    test_print("p_from_z")
+
+    """ grav """
+    grav = gsw.grav(gsw_cv.lat_chck_cast, gsw_cv.p_chck_cast )
+    test_print("grav")
+
+    """ distance """
+    distance = gsw.distance(gsw_cv.long_chck_cast, gsw_cv.lat_chck_cast, gsw_cv.p_chck_cast)
+    test_print("distance")
+
+#class DpthPressDist: #TODO: an attempt to create a class for Depth pressure conversions
     #"""
     #Class to convert pressure to depth and vice-versa:
 
@@ -234,6 +387,6 @@ def grav(lat, p=0): # Also in DepthPressure.py
 #if __name__=='__main__':
     #lat = 32.
     #p = [0., 15., 100., 550., 1500., 2000., 3000., 5000., 10000.]
-    #dp = DepthPressure(lat=lat, p=p)
+    #dp = DpthPressDist(lat=lat, p=p)
     #z = [-0., -14.89499448, -99.27948265, -545.44412444, -1484.209721, -1976.61994868, -2958.05761312, -4907.87772419, -9712.16369644]
-    #pd = DepthPressure(lat=lat, z=z)
+    #pd = DpthPressDist(lat=lat, z=z)
