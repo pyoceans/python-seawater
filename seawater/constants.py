eb3de7d54d24f71d2d8022212d7e61b014d14153
Filipe Fernandes <ocefpaf@gmail.com>
2011-01-25 10:37:20 -0500

    just a few cleanups

--- seawater/constants.py
+++ seawater/constants.py
@@ -3,8 +3,19 @@ Constants used
 ==============
 """
 
+C3515 = 42.914 #NOTE: reference value, not used.
+"""
+Conductivity of 42.914 [mmho cm :sup:`-1` == mS cm :sup:`-1`] at Salinity 35 psu, Temperature 15 :math:`^\\circ` C [ITPS 68] and Pressure 0 db.
+
+References
+----------
+.. [1] R.C. Millard and K. Yang 1992. "CTD Calibration and Processing Methods used by Woods Hole Oceanographic Institution" Draft April 14, 1992 (Personal communication)
+"""
+
 a = 6371000.
-""" mean radius of earth  A.E.Gill """
+"""
+mean radius of earth  A.E. Gill
+"""
 
 OMEGA = 7.292115e-5
 """
@@ -19,16 +30,6 @@ References
 .. [1] A.E. Gill 1982. p.54  eqn 3.7.15 "Atmosphere-Ocean Dynamics" Academic Press: New York. ISBN: 0-12-283522-0. page: 597
 .. [2] Groten, E., 2004: Fundamental Parameters and Current (2004) Best Estimates of the Parameters of Common Relevance to Astronomy, Geodesy, and Geodynamics. Journal of Geodesy, 77, pp. 724-797.
 """
-
-C3515 = 42.914 #FIXME: not used?
-"""
-Conductivity of 42.914 [mmho cm :sup:`-1` == mS cm :sup:`-1`] at Salinity 35 psu, Temperature 15 :math:`^\\circ` C [ITPS 68] and Pressure 0 db.
-
-References
-----------
-.. [1] R.C. Millard and K. Yang 1992. "CTD Calibration and Processing Methods used by Woods Hole Oceanographic Institution" Draft April 14, 1992 (Personal communication)
-"""
-
 
 gdef = 9.8
 """
@@ -57,22 +58,32 @@ Decibar to pascal
 """
 
 #--- only for gsw
-gamma = 2.26e-7
+gamma = 2.26e-7 #TODO: define
 """
-TODO: define Gamma
+Gamma (A.E. Gill)
 """
 
 M_S = 0.0314038218
-"""mole-weighted average atomic weight of the elements of sea salt, in units of kg mol :sup:`-1` """
+"""
+mole-weighted average atomic weight of the elements of sea salt, in units of kg mol :sup:`-1`
+"""
 
-cp0 = 3991.86795711963
-""" FIXME: from Eqn. (3.3.3) of IOC et al. (2010). """
+cp0 = 3991.86795711963 #TODO: define
+"""
+from Eqn. (3.3.3) of IOC et al. (2010).
+"""
 
-SSO = 35.16504
-""" from section 2.4 of IOC et al. (2010) """
+SSO = 35.16504 #TODO: define
+"""
+from section 2.4 of IOC et al. (2010)
+"""
 
-sfac = 0.0248826675584615
-""" TODO: sfac = 1 / (40 * ( 35.16504 / 35 ) ) """
+sfac = 0.0248826675584615 #TODO: define
+"""
+sfac = 1 / (40 * ( SSO / 35 ) )
+"""
 
 R = 8.314472
-""" the molar gas constant = 8.314472 m :sup:`2` kg s:sup:`-21 K :sup:`-1` mol :sup:`-1` """
+"""
+the molar gas constant = 8.314472 m :sup:`2` kg s:sup:`-21 K :sup:`-1` mol :sup:`-1`
+"""
