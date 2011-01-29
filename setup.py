f43e86c765ed32e310c7cd909d1a4b26a02c75fe
Filipe Fernandes <ocefpaf@gmail.com>
2011-01-29 11:33:03 -0500

    Updated with gibbs info

--- setup.py
+++ setup.py
@@ -18,18 +18,18 @@ Topic :: Software Development :: Libraries :: Python Modules
 """
 
 setup(name             = 'seawater',
-      version          = '1.0.6',
+      version          = '2.0.0',
       author           = 'Filipe Fernandes',
       author_email     = 'ocefpaf@gmail.com',
       maintainer       = 'Filipe Fernandes',
       maintainer_email = 'ocefpaf@gmail.com',
-      url              = 'http://ocefpaf.tiddlyspot.com/#python-seawater',
+      url              = 'http://pypi.python.org/pypi/seawater/',
       description      = 'Seawater Libray for Python',
       long_description = """\
-This module is a translation of the original SEAWATER-3.2 MATLAB toolkit routines for calculating the properties of sea water. It consists of a self contained library and is extremely easy to use.
+This python package contains a python translation for two Matlab user contributed toolboxes. (1) the `sewater <http://www.cmar.csiro.au/datacentre/ext_docs/seawater.htm>`_ (EOS-80) and (2) the `gibbs <http://www.teos-10.org/software.htm>`_ seawater (TEOS-10).
 """,
       download_url     = 'http://pypi.python.org/packages/source/s/seawater/',
-      packages         = ['seawater', 'seawater.extras', 'seawater.extras.waves'], 
+      #packages         = ['seawater', 'seawater.extras', 'seawater.extras.waves'],
       classifiers      = filter(None, classifiers.split("\n")),
       platforms        = 'any',
       license          = 'MIT',
