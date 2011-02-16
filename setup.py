3e5e0b9f526c6cc8e0043a1f95147be48b7792e9
Filipe Fernandes <ocefpaf@gmail.com>
2011-02-16 16:45:14 -0500

    update authors names

--- setup.py
+++ setup.py
@@ -19,14 +19,17 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 setup(name             = 'seawater',
       version          = '2.0.0',
-      author           = 'Filipe Fernandes',
+      author           = 'Filipe Fernandes, Eric Firing, Ådlandsvik Bjørn',
       author_email     = 'ocefpaf@gmail.com',
       maintainer       = 'Filipe Fernandes',
       maintainer_email = 'ocefpaf@gmail.com',
       url              = 'http://pypi.python.org/pypi/seawater/',
       description      = 'Seawater Libray for Python',
       long_description = """\
-This python package contains a python translation for two Matlab user contributed toolboxes. (1) the `seawater <http://www.cmar.csiro.au/datacentre/ext_docs/seawater.htm>`_ (EOS-80) and (2) the `gibbs <http://www.teos-10.org/software.htm>`_ seawater (TEOS-10).
+This python package contains a python translation for two Matlab user
+contributed toolboxes. (1) the
+`seawater <http://www.cmar.csiro.au/datacentre/ext_docs/seawater.htm>`_ (EOS-80)
+and (2) the `gibbs <http://www.teos-10.org/software.htm>`_ seawater (TEOS-10).
 """,
       download_url     = 'http://pypi.python.org/packages/source/s/seawater/',
       classifiers      = filter(None, classifiers.split("\n")),
