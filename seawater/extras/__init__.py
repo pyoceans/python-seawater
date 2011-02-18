91c7826872111c9e355f9938b32badda4e3c3f02
Filipe Fernandes <ocefpaf@gmail.com>
2011-02-17 19:29:09 -0500

    cleanup

--- seawater/extras/__init__.py
+++ seawater/extras/__init__.py
@@ -4,21 +4,7 @@
 seawater.extras
 ===============
 
-Just some extra handy functions to complement the origininal seawater.
+Just some extra handy functions to complement the original seawater.
 """
 
 import sw_extras
-
-__authors__    = ["Filipe Fernandes"]
-__copyright__  = ["left"]
-__credits__    = ["Filipe Fernandes"]
-__license__    = ["BSD"]
-__version__    = ["1.0.6"]
-__maintainer__ = ["Filipe Fernandes"]
-__email__      = ["ocefpaf@gmail.com"]
-__status__     = ["Production"]
-__all__        = ['sw_extras']
-__web__        = ['http://ocefpaf.tiddlyspot.com/']
-__created__    = ["14-Jan-2010"]
-__modified__   = ["24-Nov-2010"]
-__all__        = ["sw_extras"]
