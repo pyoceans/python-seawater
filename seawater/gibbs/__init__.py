ab9488077657f7cc0194c5baf9c06f3e3f100abb
Bjørn Ådlandsvik <bjorn@imr.no>
2011-03-25 09:04:10 +0100

    factored out two more submodules

--- seawater/gibbs/__init__.py
+++ seawater/gibbs/__init__.py
@@ -5,12 +5,55 @@ Gibbs Seawater Library
 
 Python version of the GSW Matlab toolbox version 2
 
+Submodules
+----------
+
+absolute_salinity
+    Absolute Salinity (SA) and Preformed Salinity (Sstar)
+conservative_temperature
+    Conservative Temperature (CT)
+conversions
+    other conversions between temperatures, salinities, pressure and height
+density25
+    density and enthalpy, based on 25 term expression for density
+water_column
+    water column properties, based on 25 term expression for density
+geostrophic
+    geostrophic streamfunctions, based on 25 term expression for density
+neutral
+    neutral and non-linear properties, based on 25 term expression
+basic_sa_t_p
+    basic thermodynamic properties in terms of (SA, t, p)
+basic_ct
+    basic thermodynamic properties in terms of CT and pt
+derivatives
+    derivatives of enthalpy, entropy, CT and pt
+earth
+    Planet Earth properties
+labfuncs
+    functions for laboratory use
+practical_salinity
+    Practical Salinity (SP), PSS-78
+library
+    Library functions of the GSW toolbox
+
+
 """
 
 from absolute_salinity import *
 from conservative_temperature import *
+from conversions import *
+from density25 import *
+#from water_column import *   # Not implemented yet
+#from geostrophic import *    # Not implemented yet
+#from neutral import *
 from basic_sa_t_p import *
 from basic_ct import *
+#from derivatives import *
+from earth import *
+#from labfuncs import *
+#from practical_salinity import *
+
 from gibbs import *
 from gibbs25 import *
 
