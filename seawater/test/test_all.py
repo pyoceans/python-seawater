9ff722b9cf923cbede007bca520cd7301175ca9f
Bjørn Ådlandsvik <bjorn@imr.no>
2011-04-06 18:41:42 +0200

    Added support for python3

--- seawater/test/test_all.py
+++ seawater/test/test_all.py
@@ -1,34 +1,41 @@
-#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """Perform all unit tests in the present dictionary"""
 
+# Last modified 2011-04-06 
+# This script should now work both with python2 and python3
+
+import sys
 import os
 import glob
+
+# Set executables for python2 and python3
+python2 = 'python'   # python 2.x
+python3 = 'python3'  # python 3.x
+
+# Choose the correct executable
+if sys.version[0] == 3:   # Version 3.x
+    python = python3
+else:
+    python = python2
 
 # Find all test scripts
 test_scripts = glob.glob('test*.py')
 test_scripts.remove('test_all.py')  # test_all.py is not a unit-test
 
-
+# Perform the unit-tests and accumulate errors
 errors = []
-# Perform the unit-tests
 for script in test_scripts:
-    print 'running %s' % script
-    a = os.system('python %s' % script)
+    sys.stdout.write('running %s\n' % script)
+    a = os.system('%s %s' % (python, script))
     if a != 0:
         errors.append(script)
 
-print "\n --- test_all.py finished ---"
-
-#
-print "  Ran %d test scripts" % len(test_scripts)
+# Display a summary
+sys.stdout.write("\n --- test_all.py finished ---\n")
+sys.stdout.write("  Ran %d test scripts\n" % len(test_scripts))
 if not errors:
-    print "  no problems"
+    sys.stdout.write("  no problems\n")
 else:
-    print "  problems in :"
-    print errors
-    
-    
-
-
+    sys.stdout.write("  problems in :\n")
+    sys.stdout.write(str(errors))
