e09463c31ecc4f49f360493ede8f726cfcb5421c
Filipe Fernandes <ocefpaf@gmail.com>
2011-08-07 17:44:24 -0400

    Playing with the Counter class to store test results.

--- seawater/test/test_all.py
+++ seawater/test/test_all.py
@@ -5,9 +5,10 @@
 # Last modified 2011-04-06 
 # This script should now work both with python2 and python3
 
-import sys
 import os
+import sys
 import glob
+from collections import Counter
 
 # Set executables for python2 and python3
 python2 = 'python'   # python 2.x
@@ -24,18 +25,15 @@ test_scripts = glob.glob('test*.py')
 test_scripts.remove('test_all.py')  # test_all.py is not a unit-test
 
 # Perform the unit-tests and accumulate errors
-errors = []
-for script in test_scripts:
-    sys.stdout.write('running %s\n' % script)
-    a = os.system('%s %s' % (python, script))
-    if a != 0:
-        errors.append(script)
+results = Counter()
+for test in test_scripts:
+    sys.stdout.write('running %s\n' % test)
+    a = os.system('%s %s' % (python, test))
+    if not a:
+        results['succeeded'] += 1
+    results['attempted'] += 1
 
 # Display a summary
 sys.stdout.write("\n --- test_all.py finished ---\n")
 sys.stdout.write("  Ran %d test scripts\n" % len(test_scripts))
-if not errors:
-    sys.stdout.write("  no problems\n")
-else:
-    sys.stdout.write("  problems in :\n")
-    sys.stdout.write(str(errors))
+sys.stdout.write(str(results))
