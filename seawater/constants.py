5ac1d30f7c36dae03a7debb064b1fd429bcb13b6
Filipe Fernandes <ocefpaf@gmail.com>
2011-10-05 09:33:26 -0400

    Renamed a to earth_radius (constants.py.)

--- seawater/constants.py
+++ seawater/constants.py
@@ -5,16 +5,19 @@ Constants used
 
 from __future__ import division
 
-C3515 = 42.914 #NOTE: reference value, not used.
+C3515 = 42.914  # NOTE: reference value, not used.
 """
-Conductivity of 42.914 [mmho cm :sup:`-1` == mS cm :sup:`-1`] at Salinity 35 psu, Temperature 15 :math:`^\\circ` C [ITPS 68] and Pressure 0 db.
+Conductivity of 42.914 [mmho cm :sup:`-1` == mS cm :sup:`-1`] at
+Salinity 35 psu, Temperature 15 :math:`^\\circ` C [ITPS 68] and Pressure 0 db.
 
 References
 ----------
-.. [1] R.C. Millard and K. Yang 1992. "CTD Calibration and Processing Methods used by Woods Hole Oceanographic Institution" Draft April 14, 1992 (Personal communication)
+.. [1] R.C. Millard and K. Yang 1992. "CTD Calibration and Processing Methods
+used by Woods Hole Oceanographic Institution" Draft April 14, 1992
+(Personal communication)
 """
 
-a = 6371000.
+earth_radius = 6371000.
 """
 mean radius of earth  A.E. Gill
 """
@@ -29,13 +32,17 @@ Changed to a more precise value at Groten 2004
 
 References
 ----------
-.. [1] A.E. Gill 1982. p.54  eqn 3.7.15 "Atmosphere-Ocean Dynamics" Academic Press: New York. ISBN: 0-12-283522-0. page: 597
-.. [2] Groten, E., 2004: Fundamental Parameters and Current (2004) Best Estimates of the Parameters of Common Relevance to Astronomy, Geodesy, and Geodynamics. Journal of Geodesy, 77, pp. 724-797.
+.. [1] A.E. Gill 1982. p.54  eqn 3.7.15 "Atmosphere-Ocean Dynamics" Academic
+Press: New York. ISBN: 0-12-283522-0. page: 597
+.. [2] Groten, E., 2004: Fundamental Parameters and Current (2004) Best
+Estimates of the Parameters of Common Relevance to Astronomy, Geodesy, and
+Geodynamics. Journal of Geodesy, 77, pp. 724-797.
 """
 
 gdef = 9.8
 """
-Acceleration of gravity [m s :sup:`2`] used by sw.swvel and bfrq without lat info.
+Acceleration of gravity [m s :sup:`2`] used by sw.swvel and bfrq without lat
+info.
 """
 
 DEG2NM = 60
@@ -46,12 +53,14 @@ Used by sw.dist() to convert nautical miles to kilometers.
 
 References
 ----------
-.. [1] S. Pond & G.Pickard 2nd Edition 1986 Introductory Dynamical Oceanogrpahy Pergamon Press Sydney. ISBN 0-08-028728-X. page: 303
+.. [1] S. Pond & G.Pickard 2nd Edition 1986 Introductory Dynamical
+Oceanography Pergamon Press Sydney. ISBN 0-08-028728-X. page: 303
 """
 
 Kelvin = 273.15
 """
-offset to convert :math:`^\\circ` C to Kelvin. Used in the gas solubility functions.
+Offset to convert :math:`^\\circ` C to Kelvin. Used in the gas solubility
+functions.
 """
 
 db2Pascal = 1e4
@@ -60,27 +69,28 @@ Decibar to pascal
 """
 
 #--- only for gsw
-gamma = 2.26e-7 #TODO: define
+gamma = 2.26e-7  # TODO: define
 """
 Gamma (A.E. Gill)
 """
 
 M_S = 0.0314038218
 """
-mole-weighted average atomic weight of the elements of sea salt, in units of kg mol :sup:`-1`
+Mole-weighted average atomic weight of the elements of sea salt, in units of
+kg mol :sup:`-1`
 """
 
-cp0 = 3991.86795711963 #TODO: define
+cp0 = 3991.86795711963  # TODO: define
 """
 from Eqn. (3.3.3) of IOC et al. (2010).
 """
 
-SSO = 35.16504 #TODO: define
+SSO = 35.16504  # TODO: define
 """
 from section 2.4 of IOC et al. (2010)
 """
 
-sfac = 0.0248826675584615 #TODO: define
+sfac = 0.0248826675584615  # TODO: define
 """
 sfac = 1 / (40 * ( SSO / 35 ) )
 """
@@ -95,7 +105,7 @@ r1 = 0.35
 TODO
 """
 
-uPS = 35.16504/35
+uPS = 35.16504 / 35
 """
 The unit conversion factor for salinities (35.16504/35) g/kg (Millero et
 al., 2008). Reference Salinity SR is uPS times Practical Salinity SP.
