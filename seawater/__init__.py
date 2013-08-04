ff2d9b276928d2bf06ae81b1fa243ee2816cd694
ocefpaf <ocefpaf@gmail.com>
2013-08-03 22:00:51 -0300

    Update to reflect some small re-factoring.

--- seawater/__init__.py
+++ seawater/__init__.py
@@ -1,14 +1,3 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["csiro", "extras"]
-__authors__ = 'Filipe Fernandes'
-__created_ = "14-Jan-2010"
-__email__ = "ocefpaf@gmail.com"
-__license__ = "MIT"
-__maintainer__ = "Filipe Fernandes"
-__modified__ = "16-Mar-2013"
-__status__ = "Production"
-__version__ = "2.0.0"
-
-import csiro
-import extras
+from csiro import *
