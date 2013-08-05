a5ba78cfaec6d9f732d083e4679bd9810dd10389
ocefpaf <ocefpaf@gmail.com>
2013-08-05 18:04:56 -0300

    Moved bfrq from eos80.py to geostrophic.py.

--- seawater/__init__.py
+++ seawater/__init__.py
@@ -2,8 +2,8 @@
 
 __version__ = '3.3'
 
-from geostrophic import svan, gpan, gvel
+from geostrophic import bfrq, svan, gpan, gvel
 from extras import dist, f, satAr, satN2, satO2, swvel
-from library import cndr, salds, salrp, salrt, seck, sals, T68conv, T90conv
-from eos80 import (adtg, alpha, aonb, beta, bfrq, dpth, g, salt, fp, svel,
-                   pres, dens0, smow, dens, pden, cp, ptmp, temp)
+from library import cndr, salds, salrp, salrt, seck, sals, smow
+from eos80 import (adtg, alpha, aonb, beta, dpth, g, salt, fp, svel,
+                   pres, dens0, dens, pden, cp, ptmp, temp)
