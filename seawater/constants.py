8d8092dc42b4c8a67620ecc9bf6a192692c8e6f3
ocefpaf <ocefpaf@gmail.com>
2013-08-05 18:05:26 -0300

    Using the same OMEGA as in the Matlab toolbox. Added c3515.

--- seawater/constants.py
+++ seawater/constants.py
@@ -25,8 +25,10 @@ gdef = 9.8
 # 1 nm = 1.8520 km
 DEG2NM, NM2KM = 60., 1.8520
 # Sidereal day = 23.9344696 hours.
-OMEGA = 7.292115e-5
+OMEGA = 7.292e-5  # 7.292115e-5
 # Mean radius of earth [m] A.E. Gill.
 earth_radius = 6371000.
 # Angle conversions.
 deg2rad, rad2deg = pi / 180.0,  180.0 / pi
+# Conductivity at S=35 psu , T=15 C [ITPS 68] and P=0 dbar.
+c3515 = 42.914
