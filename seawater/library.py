a62e9d0afd7d046dfcf490bf1ce8a0054a032bbc
ocefpaf <ocefpaf@gmail.com>
2013-08-06 10:07:29 -0300

    Added a atleast_2d akin to numpy but with single dimension last instead of first.

--- seawater/library.py
+++ seawater/library.py
@@ -542,6 +542,26 @@ def T90conv(t, t_type='T68'):
 
     return T90
 
+
+def atleast_2d(*arys):
+    """Same as numpy atleast_2d, but with the single dimension last,
+    instead of first."""
+    res = []
+    for ary in arys:
+        ary = np.asanyarray(ary)
+        if len(ary.shape) == 0 :
+            result = ary.reshape(1, 1)
+        elif len(ary.shape) == 1 :
+            result = ary[:, np.newaxis]
+        else :
+            result = ary
+        res.append(result)
+    if len(res) == 1:
+        return res[0]
+    else:
+        return res
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
