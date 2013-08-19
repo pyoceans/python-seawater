1df6a19c8a4cfcc46a033e995165914d35857870
ocefpaf <ocefpaf@gmail.com>
2013-08-19 19:44:02 -0300

    Change octave-devel name.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -13,4 +13,5 @@ sudo apt-get install libatlas-base-dev liblapack-dev
 sudo apt-get install python-numpy
 sudo apt-get install python-scipy
 sudo apt-get install python-matplotlib
-sudo apt-get install octave octave-devel
+sudo apt-get install octave
+sudo apt-get install octave-pkg-dev
