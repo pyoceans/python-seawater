19e0843fa13adc2dd41c647f4db29b437ce55764
ocefpaf <ocefpaf@gmail.com>
2013-08-20 14:16:35 -0300

    Checking if travis as a working pandas.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -8,12 +8,21 @@ echo "Using SITE_PKG_DIR: $SITE_PKG_DIR"
 # Workaround for travis ignoring system_site_packages in travis.yml
 rm -f $VIRTUAL_ENV/lib/python$TRAVIS_PYTHON_VERSION/no-global-site-packages.txt
 
-sudo apt-get install libhdf5-serial-dev hdf5-tools
-sudo apt-get install libatlas-base-dev liblapack-dev
-sudo apt-get install python-numpy
-sudo apt-get install python-scipy
-sudo apt-get install octave
-sudo apt-get install octave-pkg-dev
+sudo apt-get update -qq
+# sudo apt-get install -qq build-dep python-scipy
+# sudo apt-get install -qq build-dep python-numpy
+# sudo apt-get install -qq build-dep python-pandas
+# sudo apt-get install -qq build-essential
+# sudo apt-get install -qq libhdf5-serial-dev hdf5-tools
+# sudo apt-get install -qq libatlas-base-dev liblapack-dev
 
-sudo apt-get install python3-numpy
-sudo apt-get install python3-scipy
+sudo apt-get install -qq python-numpy
+sudo apt-get install -qq python-scipy
+sudo apt-get install -qq python-pandas
+
+sudo apt-get install -qq python3-numpy
+sudo apt-get install -qq python3-scipy
+sudo apt-get install -qq python3-pandas
+
+sudo apt-get install -qq octave
+sudo apt-get install -qq octave-pkg-dev
