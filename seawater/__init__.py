324efed47b4706cea91076c4774622a5b14acae9
ocefpaf <ocefpaf@gmail.com>
2014-07-24 17:42:24 -0300

    Import test.

--- seawater/__init__.py
+++ seawater/__init__.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import
 
 __version__ = '3.3.2'
 
+from . import test
 from .geostrophic import bfrq, svan, gpan, gvel
 from .extras import dist, f, satAr, satN2, satO2, swvel
 from .library import cndr, salds, salrp, salrt, seck, sals, smow
 from .eos80 import (adtg, alpha, aonb, beta, dpth, g, salt, fp, svel,
                     pres, dens0, dens, pden, cp, ptmp, temp)
-from .test import test_result_comparison
